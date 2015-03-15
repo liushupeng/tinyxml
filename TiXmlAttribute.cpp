0e0a9a46315b6f1289d28bd4ab3be54a174e4127
liushupeng <desert_fox@foxmail.com>
2015-03-15 15:23:49 +0800

    Update TiXmlAttribute.cpp

--- TiXmlAttribute.cpp
+++ TiXmlAttribute.cpp
@@ -65,3 +65,114 @@ private:
 };
 
 /* 方法 */
+
+/* 将类似于 name=test的内容以key-value的形式解析出来 */
+const char* TiXmlAttribute::Parse( const char* p, TiXmlParsingData* data, TiXmlEncoding encoding )
+{
+  p = SkipWhiteSpace( p, encoding );
+  if ( !p || !*p ) return 0;
+
+  if ( data )
+  {
+    data->Stamp( p, encoding );
+    location = data->Cursor();
+  }
+  // Read the name, the '=' and the value.
+  const char* pErr = p; 
+  p = ReadName( p, &name, encoding );
+  if ( !p || !*p )
+  {
+    if ( document ) document->SetError( TIXML_ERROR_READING_ATTRIBUTES, pErr, data, encoding );
+    return 0;
+  }
+  p = SkipWhiteSpace( p, encoding );
+  if ( !p || !*p || *p != '=' )
+  {
+    if ( document ) document->SetError( TIXML_ERROR_READING_ATTRIBUTES, p, data, encoding );
+    return 0;
+  }
+    ++p;  // skip '='
+  p = SkipWhiteSpace( p, encoding );
+  if ( !p || !*p )
+  {
+    if ( document ) document->SetError( TIXML_ERROR_READING_ATTRIBUTES, p, data, encoding );
+    return 0;
+  }
+
+  const char* end;
+  const char SINGLE_QUOTE = '\'';
+  const char DOUBLE_QUOTE = '\"';
+
+  if ( *p == SINGLE_QUOTE )
+  {
+    ++p;
+    end = "\'";   // single quote in string
+    p = ReadText( p, &value, false, end, false, encoding );
+  }
+  else if ( *p == DOUBLE_QUOTE )
+  {
+    ++p;
+    end = "\"";   // double quote in string
+    p = ReadText( p, &value, false, end, false, encoding );
+  }
+    else
+  {
+    value = "";
+    while (p && *p && !IsWhiteSpace(*p ) && *p != '/' && *p != '>' )
+    {
+      if ( *p == SINGLE_QUOTE || *p == DOUBLE_QUOTE )
+      {
+        if ( document ) document->SetError( TIXML_ERROR_READING_ATTRIBUTES, p, data, encoding );
+        return 0;
+      }
+      value += *p;
+      ++p;
+    }
+  }
+  return p;
+}
+
+/* 更安全的一种方法(未必) */
+int TiXmlAttribute::QueryIntValue( int* ival ) const
+{
+  if ( TIXML_SSCANF( value.c_str(), "%d", ival ) == 1 )
+    return TIXML_SUCCESS;
+  return TIXML_WRONG_TYPE;
+}
+int TiXmlAttribute::QueryDoubleValue( double* dval ) const
+{
+  if ( TIXML_SSCANF( value.c_str(), "%lf", dval ) == 1 )
+    return TIXML_SUCCESS;
+  return TIXML_WRONG_TYPE;
+}
+
+/* Print()函数的实现 */
+void TiXmlAttribute::Print( FILE* cfile, int /*depth*/, TIXML_STRING* str ) const
+{
+  TIXML_STRING n, v;
+
+  EncodeString( name, &n );
+  EncodeString( value, &v );
+
+  if (value.find ('\"') == TIXML_STRING::npos) /* TIXML_STRING::npos 表示“未找到”，值为static const unsigned -1 */
+  {
+    if ( cfile ) 
+    {
+      fprintf (cfile, "%s=\"%s\"", n.c_str(), v.c_str() );
+    }
+    if ( str ) 
+    {
+      (*str) += n; (*str) += "=\""; (*str) += v; (*str) += "\"";
+    }
+  }
+  else {
+    if ( cfile ) 
+    {
+      fprintf (cfile, "%s='%s'", n.c_str(), v.c_str() );
+    }
+    if ( str ) 
+    {
+      (*str) += n; (*str) += "='"; (*str) += v; (*str) += "'";
+    }
+  }
+}
