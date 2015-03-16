84da9d007de0319a019730381f05fba97841cf30
liushupeng <desert_fox@foxmail.com>
2015-03-16 20:49:06 +0800

    Update TiXmlNode.cpp

--- TiXmlNode.cpp
+++ TiXmlNode.cpp
@@ -1,4 +1,6 @@
-/* TiXmlNode类是它的派生类的基础类，方法对应着不同的子类，会有不同的含义 */
+/* TiXmlNode类是它的派生类的基础类，方法对应着不同的子类，会有不同的含义 
+ * 个人理解，它就像Animal类，派生出来的子类是Dog, Bird, Cat
+ */
 class TiXmlNode : public TiXmlBase
 {
   friend class TiXmlDocument;
@@ -20,243 +22,186 @@ public:
 
   virtual ~TiXmlNode();
   
-    const char *Value() const { return value.c_str (); }
-
-    #ifdef TIXML_USE_STL
-  /** Return Value() as a std::string. If you only use STL,
-      this is more efficient than calling Value().
-    Only available in STL mode.
-  */
-  const std::string& ValueStr() const { return value; }
-  #endif
-
+  const char *Value() const { return value.c_str (); }
   const TIXML_STRING& ValueTStr() const { return value; }
 
-  /** Changes the value of the node. Defined as:
-    @verbatim
+  /*Changes the value of the node. (不同的子类，对应的value是不同的)
+    Defined as:
     Document: filename of the xml file
     Element:  name of the element
     Comment:  the comment text
     Unknown:  the tag contents
     Text:   the text string
-    @endverbatim
   */
   void SetValue(const char * _value) { value = _value;}
   
-    /// Delete all the children of this node. Does not affect 'this'.
+  /* 删除当前节点所有的子节点，但是对当前节点没有影响 */
   void Clear();
 
-  /// One step up the DOM.
-  TiXmlNode* Parent()             { return parent; }
-  const TiXmlNode* Parent() const       { return parent; }
-
-  const TiXmlNode* FirstChild() const   { return firstChild; }  ///< The first child of this node. Will be null if there are no children.
-  TiXmlNode* FirstChild()           { return firstChild; }
-  const TiXmlNode* FirstChild( const char * value ) const;      ///< The first child of this node with the matching 'value'. Will be null if none found.
-  /// The first child of this node with the matching 'value'. Will be null if none found.
-  TiXmlNode* FirstChild( const char * _value ) {
-    // Call through to the const version - safe since nothing is changed. Exiting syntax: cast this to a const (always safe)
-    // call the method, cast the return back to non-const.
+  /* 返回父节点 */
+  TiXmlNode* Parent() { return parent; }
+  const TiXmlNode* Parent() const { return parent; }
+  
+  /* 返回第一个孩子结点 */
+  const TiXmlNode* FirstChild() const { return firstChild; }
+  TiXmlNode* FirstChild() { return firstChild; }
+  
+  /* 返回匹配value的第一个孩子结点 */
+  const TiXmlNode* FirstChild( const char * value ) const;
+  TiXmlNode* FirstChild( const char * _value ) 
+  {
     return const_cast< TiXmlNode* > ((const_cast< const TiXmlNode* >(this))->FirstChild( _value ));
   }
-  const TiXmlNode* LastChild() const  { return lastChild; }   /// The last child of this node. Will be null if there are no children.
-  TiXmlNode* LastChild()  { return lastChild; }
-
-  const TiXmlNode* LastChild( const char * value ) const;     /// The last child of this node matching 'value'. Will be null if there are no children.
   
-   TiXmlNode* LastChild( const char * _value ) {
+  /* 返回最后一个孩子结点 */
+  const TiXmlNode* LastChild() const  { return lastChild; } 
+  TiXmlNode* LastChild()  { return lastChild; }
+  
+  /* 返回匹配value的最后一个孩子结点 */
+  const TiXmlNode* LastChild( const char * value ) const; 
+  TiXmlNode* LastChild( const char * _value )
+  {
     return const_cast< TiXmlNode* > ((const_cast< const TiXmlNode* >(this))->LastChild( _value ));
   }
-
-    #ifdef TIXML_USE_STL
-  const TiXmlNode* FirstChild( const std::string& _value ) const  { return FirstChild (_value.c_str ());  } ///< STL std::string form.
-  TiXmlNode* FirstChild( const std::string& _value )        { return FirstChild (_value.c_str ());  } ///< STL std::string form.
-  const TiXmlNode* LastChild( const std::string& _value ) const { return LastChild (_value.c_str ()); } ///< STL std::string form.
-  TiXmlNode* LastChild( const std::string& _value )       { return LastChild (_value.c_str ()); } ///< STL std::string form.
-  #endif
   
-    /** An alternate way to walk the children of a node.
-    One way to iterate over nodes is:
-    @verbatim
-      for( child = parent->FirstChild(); child; child = child->NextSibling() )
-    @endverbatim
-
-    IterateChildren does the same thing with the syntax:
-    @verbatim
-      child = 0;
-      while( child = parent->IterateChildren( child ) )
-    @endverbatim
-
-    IterateChildren takes the previous child as input and finds
-    the next one. If the previous child is null, it returns the
-    first. IterateChildren will return null when done.
-  */
+  /* 一种遍历孩子结点的方法：
+   * for( child = parent->FirstChild(); child; child = child->NextSibling())
+   * 
+   * IterateChildren()也是做同样的事，使用方式如下：
+   * while( child = parent->IterateChildren( child ) )
+   * 它使用前一个孩子节点作为输入，通过这个节点，找到下一个。如果前一个节点是NULL，它会返回第一个节点
+   */
   const TiXmlNode* IterateChildren( const TiXmlNode* previous ) const;
-  TiXmlNode* IterateChildren( const TiXmlNode* previous ) {
+  TiXmlNode* IterateChildren( const TiXmlNode* previous ) 
+  {
     return const_cast< TiXmlNode* >( (const_cast< const TiXmlNode* >(this))->IterateChildren( previous ) );
   }
   
-    /// This flavor of IterateChildren searches for children with a particular 'value'
+  /* 遍历孩子结点的时候，需要匹配value值 */
   const TiXmlNode* IterateChildren( const char * value, const TiXmlNode* previous ) const;
-  TiXmlNode* IterateChildren( const char * _value, const TiXmlNode* previous ) {
-    return const_cast< TiXmlNode* >( (const_cast< const TiXmlNode* >(this))->IterateChildren( _value, previous ) );
+  TiXmlNode* IterateChildren( const char * _value, const TiXmlNode* previous ) 
+  {
+    return const_cast< TiXmlNode* >((const_cast< const TiXmlNode* >(this))->IterateChildren( _value, previous ));
   }
 
-    #ifdef TIXML_USE_STL
-  const TiXmlNode* IterateChildren( const std::string& _value, const TiXmlNode* previous ) const  { return IterateChildren (_value.c_str (), previous); } ///< STL std::string form.
-  TiXmlNode* IterateChildren( const std::string& _value, const TiXmlNode* previous ) {  return IterateChildren (_value.c_str (), previous); } ///< STL std::string form.
-  #endif
-
-  /** Add a new node related to this. Adds a child past the LastChild.
-    Returns a pointer to the new object or NULL if an error occured.
-  */
+  /* 将当前节点加入到子节点末尾的两种方法，参数类型不同而已 */
   TiXmlNode* InsertEndChild( const TiXmlNode& addThis );
-  
-    /** Add a new node related to this. Adds a child past the LastChild.
-
-    NOTE: the node to be added is passed by pointer, and will be
-    henceforth owned (and deleted) by tinyXml. This method is efficient
-    and avoids an extra copy, but should be used with care as it
-    uses a different memory model than the other insert functions.
-
-    @sa InsertEndChild
-  */
   TiXmlNode* LinkEndChild( TiXmlNode* addThis );
 
-  /** Add a new node related to this. Adds a child before the specified child.
-    Returns a pointer to the new object or NULL if an error occured.
-  */
+  /* 在当前节点前面加入子节点 */
   TiXmlNode* InsertBeforeChild( TiXmlNode* beforeThis, const TiXmlNode& addThis );
-
-  /** Add a new node related to this. Adds a child after the specified child.
-    Returns a pointer to the new object or NULL if an error occured.
-  */
+  /* 在当前节点后面加入子节点 */
   TiXmlNode* InsertAfterChild(  TiXmlNode* afterThis, const TiXmlNode& addThis );
 
-  /** Replace a child of this node.
-    Returns a pointer to the new object or NULL if an error occured.
-  */
+  /* 替换当前子节点 */
   TiXmlNode* ReplaceChild( TiXmlNode* replaceThis, const TiXmlNode& withThis );
-  
-   /// Delete a child of this node.
+  /* 删除当前子节点 */
   bool RemoveChild( TiXmlNode* removeThis );
 
-  /// Navigate to a sibling node.
-  const TiXmlNode* PreviousSibling() const      { return prev; }
-  TiXmlNode* PreviousSibling()            { return prev; }
+  /* 同级的前一个节点 */
+  const TiXmlNode* PreviousSibling() const { return prev; }
+  TiXmlNode* PreviousSibling() { return prev; }
 
-  /// Navigate to a sibling node.
   const TiXmlNode* PreviousSibling( const char * ) const;
-  TiXmlNode* PreviousSibling( const char *_prev ) {
+  TiXmlNode* PreviousSibling( const char *_prev ) 
+  {
     return const_cast< TiXmlNode* >( (const_cast< const TiXmlNode* >(this))->PreviousSibling( _prev ) );
   }
   
-    /// Navigate to a sibling node.
-  const TiXmlNode* NextSibling() const        { return next; }
-  TiXmlNode* NextSibling()              { return next; }
+  /* 同级的下一个节点 */
+  const TiXmlNode* NextSibling() const { return next; }
+  TiXmlNode* NextSibling() { return next; }
 
-  /// Navigate to a sibling node with the given 'value'.
   const TiXmlNode* NextSibling( const char * ) const;
   TiXmlNode* NextSibling( const char* _next ) {
     return const_cast< TiXmlNode* >( (const_cast< const TiXmlNode* >(this))->NextSibling( _next ) );
   }
 
-  /** Convenience function to get through elements.
-    Calls NextSibling and ToElement. Will skip all non-Element
-    nodes. Returns 0 if there is not another element.
-  */
+  /* 下一个同级元素 */
   const TiXmlElement* NextSiblingElement() const;
-  TiXmlElement* NextSiblingElement() {
-    return const_cast< TiXmlElement* >( (const_cast< const TiXmlNode* >(this))->NextSiblingElement() );
+  TiXmlElement* NextSiblingElement()
+  {
+    return const_cast< TiXmlElement* >((const_cast< const TiXmlNode* >(this))->NextSiblingElement());
   }
-
-  /** Convenience function to get through elements.
-    Calls NextSibling and ToElement. Will skip all non-Element
-    nodes. Returns 0 if there is not another element.
-  */
+  
   const TiXmlElement* NextSiblingElement( const char * ) const;
-  TiXmlElement* NextSiblingElement( const char *_next ) {
+  TiXmlElement* NextSiblingElement( const char *_next ) 
+  {
     return const_cast< TiXmlElement* >( (const_cast< const TiXmlNode* >(this))->NextSiblingElement( _next ) );
   }
   
-    /// Convenience function to get through elements.
   const TiXmlElement* FirstChildElement() const;
-  TiXmlElement* FirstChildElement() {
+  TiXmlElement* FirstChildElement() 
+  {
     return const_cast< TiXmlElement* >( (const_cast< const TiXmlNode* >(this))->FirstChildElement() );
   }
 
-  /// Convenience function to get through elements.
   const TiXmlElement* FirstChildElement( const char * _value ) const;
-  TiXmlElement* FirstChildElement( const char * _value ) {
+  TiXmlElement* FirstChildElement( const char * _value ) 
+  {
     return const_cast< TiXmlElement* >( (const_cast< const TiXmlNode* >(this))->FirstChildElement( _value ) );
   }
 
-    #ifdef TIXML_USE_STL
-  const TiXmlElement* FirstChildElement( const std::string& _value ) const  { return FirstChildElement (_value.c_str ()); } ///< STL std::string form.
-  TiXmlElement* FirstChildElement( const std::string& _value )        { return FirstChildElement (_value.c_str ()); } ///< STL std::string form.
-  #endif
-
-  /** Query the type (as an enumerated value, above) of this node.
-    The possible types are: TINYXML_DOCUMENT, TINYXML_ELEMENT, TINYXML_COMMENT,
-                TINYXML_UNKNOWN, TINYXML_TEXT, and TINYXML_DECLARATION.
-  */
-  int Type() const  { return type; }
+  /* 可选值列表：
+   * TINYXML_DOCUMENT, 
+   * TINYXML_ELEMENT, 
+   * TINYXML_COMMENT,
+   * TINYXML_UNKNOWN, 
+   * TINYXML_TEXT, 
+   *TINYXML_DECLARATION.
+   */
+  int Type() const { return type; }
   
-    const TiXmlDocument* GetDocument() const;
-  TiXmlDocument* GetDocument() {
+  const TiXmlDocument* GetDocument() const;
+  TiXmlDocument* GetDocument()
+  {
     return const_cast< TiXmlDocument* >( (const_cast< const TiXmlNode* >(this))->GetDocument() );
   }
 
-  /// Returns true if this node has no children.
-  bool NoChildren() const           { return !firstChild; }
-
-  virtual const TiXmlDocument*    ToDocument()    const { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual const TiXmlElement*     ToElement()     const { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual const TiXmlComment*     ToComment()     const { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual const TiXmlUnknown*     ToUnknown()     const { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual const TiXmlText*        ToText()        const { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual const TiXmlDeclaration* ToDeclaration() const { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-
-  virtual TiXmlDocument*          ToDocument()    { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual TiXmlElement*           ToElement()     { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
+  /* 判断是否存在子节点 */
+  bool NoChildren() const { return !firstChild; }
   
-    virtual TiXmlComment*           ToComment()     { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual TiXmlUnknown*           ToUnknown()     { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual TiXmlText*              ToText()        { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
-  virtual TiXmlDeclaration*       ToDeclaration() { return 0; } ///< Cast to a more defined type. Will return null if not of the requested type.
+  /* 类型转换 */
+  virtual const TiXmlDocument*    ToDocument()    const { return 0; }
+  virtual const TiXmlElement*     ToElement()     const { return 0; }
+  virtual const TiXmlComment*     ToComment()     const { return 0; } 
+  virtual const TiXmlUnknown*     ToUnknown()     const { return 0; } 
+  virtual const TiXmlText*        ToText()        const { return 0; } 
+  virtual const TiXmlDeclaration* ToDeclaration() const { return 0; } 
 
-  /** Create an exact duplicate of this node and return it. The memory must be deleted
-    by the caller. 
-  */
+  virtual TiXmlDocument*          ToDocument()    { return 0; } 
+  virtual TiXmlElement*           ToElement()     { return 0; } 
+  
+  virtual TiXmlComment*           ToComment()     { return 0; } 
+  virtual TiXmlUnknown*           ToUnknown()     { return 0; }
+  virtual TiXmlText*              ToText()        { return 0; }
+  virtual TiXmlDeclaration*       ToDeclaration() { return 0; } 
+
+  /* 克隆函数 */
   virtual TiXmlNode* Clone() const = 0;
   
   protected:
-  TiXmlNode( NodeType _type );
+  TiXmlNode( NodeType _type);
 
-  // Copy to the allocated object. Shared functionality between Clone, Copy constructor,
-  // and the assignment operator.
   void CopyTo( TiXmlNode* target ) const;
 
-  #ifdef TIXML_USE_STL
-      // The real work of the input operator.
-  virtual void StreamIn( std::istream* in, TIXML_STRING* tag ) = 0;
-  #endif
-
-  // Figure out what is at *p, and parse it. Returns null if it is not an xml node.
+  /* 验证当前节点是否符合XML格式 */
   TiXmlNode* Identify( const char* start, TiXmlEncoding encoding );
 
-  TiXmlNode*    parent;
+  /* 数据成员 */
+  TiXmlNode*  parent;
   NodeType    type;
 
-  TiXmlNode*    firstChild;
-  TiXmlNode*    lastChild;
+  TiXmlNode*  firstChild;
+  TiXmlNode*  lastChild;
 
   TIXML_STRING  value;
 
-  TiXmlNode*    prev;
-  TiXmlNode*    next;
+  TiXmlNode*  prev;
+  TiXmlNode*  next;
 
 private:
-  TiXmlNode( const TiXmlNode& );        // not implemented.
-  void operator=( const TiXmlNode& base );  // not allowed.
+  /* 拷贝构造函数和复制运算符不允许调用 */
+  TiXmlNode( const TiXmlNode& );
+  void operator=( const TiXmlNode& base ); 
 };
